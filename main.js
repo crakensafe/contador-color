4d4b1e7f4a6c2126f3b3abf483e00b242d615132
crakensafe <crakensafe@icloud.com>
2025-04-18 12:51:15 -0600

    firts commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const btnDecrease = document.querySelector(".btn-decrease");
+const btnReset = document.querySelector(".btn-reset");
+const btnIncrease = document.querySelector(".btn-increase");
+
+
+btnDecrease.addEventListener('click', () => {
+    console.log("diste click");
+});
